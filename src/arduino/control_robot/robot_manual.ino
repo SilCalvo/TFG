900c0a52945c8ddafa1b180ada2d4fab1cd1c282
SilCalvo <144826059+SilCalvo@users.noreply.github.com>
2026-05-17 17:25:23 +0200

    Implement smooth manual control for servos

--- src/arduino/control_robot/robot_manual.ino
+++ src/arduino/control_robot/robot_manual.ino
@@ -6,6 +6,10 @@
  * Updates the servos only if the change is significant (>= 1 degree) to prevent jitter/vibration caused by signal noise.
  * Sends the current joint angles via Serial to ROS 2 (Format: "j1,j2,j3,j4,j5\n") so the PC knows the robot's pose.
  */
+
+unsigned long last_manual_update = millis();
+const int MANUAL_INTERVAL = 200;
+
 int parse_potenciometer(int pin) {
   int read = analogRead(pin);
   int angle = map(read, 0, 1023, 0, 180);
@@ -14,21 +18,47 @@ int parse_potenciometer(int pin) {
   return angle;
 }
 
+void smooth_manual_control() {
+
+  for (int i = 0; i < NUMBER_SERVOS; i++) {
+
+    int target_angle = parse_potenciometer(ROBOT_POTENCIOMETERS[i]);
+
+    while (abs(target_angle - actual_position[i]) > 5) {
+
+      if (target_angle > actual_position[i]) {
+        actual_position[i] += 5;
+      }
+      else {
+        actual_position[i] -= 5;
+      }
+      // servos[i].write(actual_position[i]);
+      delay(20); // suavidad
+    }
+    actual_position[i] = target_angle;
+    // servos[i].write(actual_position[i]);
+  }
+}
+
 void manual_control(){
+
+  if (millis() - last_manual_update < MANUAL_INTERVAL) {
+    return;
+  }
+  last_manual_update = millis();
 
   // Control servos
   for (int i = 0; i < NUMBER_SERVOS; i++) {
     int angle = parse_potenciometer(ROBOT_POTENCIOMETERS[i]);
     if (abs(angle - actual_position[i]) >=1) {
-      servos[i].write(angle); 
+      //servos[i].write(angle); 
       actual_position[i]=angle;
     }
-    delay(10);
   }
 
-  
-  if(wall_button.isPressed()) {
+  if(wall == true) {
     Serial.print("WALL");
+    wall= false;
   }
   Serial.print("(");
 
@@ -42,7 +72,9 @@ void manual_control(){
   }
   Serial.print(")");
   
+  
   // End the message with a New Line so ROS2 knows the packet is finished
   Serial.println(); 
-  delay(100);
+  digitalWrite(LED_WALL, LOW);
+
 }
